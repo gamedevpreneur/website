185e5042918be6f0dd51206339dd64f468bf9c67
sainthkh <sainthkh@naver.com>
2018-08-23 17:05:03 +0900

    Added "compileAll" feature to compiler.

--- GDP/compiler/bin.js
+++ GDP/compiler/bin.js
@@ -1,27 +1,45 @@
-const filename = process.argv[2];
-
-if (filename == '') {
-    console.log("Usage: npm run compile [filename]")
-    return;
-}
+const compilePost = require('./index')
 
 const fs = require('fs');
 const yamlFront = require('yaml-front-matter');
 const moment = require('moment');
-const compile = require('./index')
-
-var post = fs.readFileSync('./md/' + filename).toString();
-post = yamlFront.loadFront(post);
-post.content = compile(post.__content)
-post.time = moment(post.time).format();
-
+const walk = require('walk');
 const Database = require('better-sqlite3');
+
+
+const filename = process.argv[2];
+var compileAll = false;
+
+if (!filename) {
+    compileAll = true;
+}
+
 const db = new Database('posts.db');
 
-var row = db.prepare('SELECT * from posts where slug = ?').get(post.slug);
+function compile(filePath) {
+    var post = fs.readFileSync(filePath).toString();
+    post = yamlFront.loadFront(post);
+    post.content = compilePost(post.__content)
+    post.time = moment(post.time).format();
 
-if(!row) {
-    db.prepare('INSERT INTO posts VALUES(@title, @description, @content, @slug, @time, @src)').run(post);
-} else {
-    db.prepare('UPDATE posts SET title=@title, description=@description, content=@content, time=@time, src=@src WHERE slug=@slug').run(post);
+    var row = db.prepare('SELECT * from posts where slug = ?').get(post.slug);
+
+    if(!row) {
+        db.prepare('INSERT INTO posts VALUES(@title, @description, @content, @slug, @time, @src)').run(post);
+    } else {
+        db.prepare('UPDATE posts SET title=@title, description=@description, content=@content, time=@time, src=@src WHERE slug=@slug').run(post);
+    }
 }
+
+if (!compileAll) {
+    compile('./md/' + filename)
+} else {
+    walker = walk.walk('./md')
+
+    walker.on("file", function(root, fileStats, next) {
+        compile(root + '/' + fileStats.name);
+        next();
+    })
+}
+
+
