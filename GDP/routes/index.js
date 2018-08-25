08571779e3f5cf1b8067ae718e73f08e84edb592
sainthkh <sainthkh@naver.com>
2018-08-25 16:05:47 +0900

    Added basic comment system.

--- GDP/routes/index.js
+++ GDP/routes/index.js
@@ -3,12 +3,21 @@ var fs = require('fs');
 var createError = require('http-errors');
 const Database = require('better-sqlite3');
 const db = new Database('posts.db');
+const commentsDB = new Database('comments.db');
 var router = express.Router();
 
-/* GET home page. */
+router.post('/comment', function(req, res, next) {
+  var { comment, author, email, url, slug, parentID } = req.body;
+  commentsDB.prepare('INSERT INTO comments VALUES(?, ?, ?, ?, ?, ?, ?)')
+    .run(comment, author, email, url, (new Date()).toString(), slug, parentID)
+})
+
+
 router.use('/:slug', function(req, res, next) {
-  var slug = req.params['postId'];
+  var slug = req.params['slug'];
   var post = db.prepare('SELECT * from posts where slug = ?').get(slug);
+  var comments = commentsDB.prepare('SELECT * from comments where slug = ?').all(slug);
+  post.comments = comments;
 
   if(post) {
     res.render('content', post);
