591569d1cf2209045e6e114c9d81035e479f49e5
sainthkh <sainthkh@naver.com>
2018-08-29 16:17:37 +0900

    Added comment system.

--- GDP/assets/js/front.js
+++ GDP/assets/js/front.js
@@ -5,3 +5,50 @@ $('.show-answer').on('click', function() {
     $(this).addClass('hide');
     $('#' + divID).removeClass('hide');
 })
+
+$('.comment-form').on('submit', commentAction);
+
+function commentAction(e) {
+    var form = $(this);
+    var xhr = new XMLHttpRequest();
+    xhr.open("POST", '/comment', true);
+    xhr.setRequestHeader("Content-Type", "application/x-www-form-urlencoded");
+    
+    xhr.onreadystatechange = function() {//Call a function when the state changes.
+        if(this.readyState == XMLHttpRequest.DONE && this.status == 200) {
+            var result = JSON.parse(this.response);
+            var id = result.ID;
+            
+            var list = form.closest('.comment-root').children('.comment-list');
+            list.append(result.html);
+            document.getElementById('comment-' + id).scrollIntoView();
+            $('#comment-' + id).addClass('focus-bg');
+            setTimeout(() => {
+                $('#comment-' + id).removeClass('focus-bg');
+            }, 5000);
+
+            e.preventDefault();
+        }
+    }
+
+    xhr.send($(this).serialize());
+
+    $(this).find('textarea, input').val('');
+
+    e.preventDefault();
+}
+
+$('.reply-button').on('click', function(e) {
+    if (!$(this).data('reply')) {
+        var body = $(this).closest('.comment-body');
+        var form = $('#respond').clone();
+        var ID = body.data('id');
+        form.find('[name="parentID"]').val(ID);
+        form.find('.comment-form').on('submit', commentAction);
+        body.append(form);
+    } else {
+        $(this).closest('.comment-body').find('#respond').remove();
+    }
+    $(this).data('reply', !$(this).data('reply'));
+    e.preventDefault();
+})
--- GDP/routes/index.js
+++ GDP/routes/index.js
@@ -1,4 +1,6 @@
 var express = require('express');
+var md5 = require('md5');
+var hbs = require('hbs');
 var fs = require('fs');
 var createError = require('http-errors');
 const Database = require('better-sqlite3');
@@ -8,16 +10,37 @@ var router = express.Router();
 
 router.post('/comment', function(req, res, next) {
   var { comment, author, email, url, slug, parentID } = req.body;
-  commentsDB.prepare('INSERT INTO comments VALUES(?, ?, ?, ?, ?, ?, ?)')
-    .run(comment, author, email, url, (new Date()).toString(), slug, parentID)
+  var imageUrl = md5(email.trim().toLowerCase());
+  var info = commentsDB.prepare('INSERT INTO comments VALUES(?, ?, ?, ?, ?, ?, ?)')
+    .run(comment, author, imageUrl, url, (new Date()).toString(), slug, parentID)
+  
+  fs.readFile('./views/partials/single-comment.hbs', (err, data) => {
+    var template = hbs.handlebars.compile(data.toString());
+
+    var ID = info.lastInsertROWID;
+    res.json({
+      ID,
+      html : template({
+        comment,
+        author,
+        imageUrl,
+        url,
+        slug,
+        parentID,
+        ID,
+      })
+    })
+
+    next();
+  })
 })
 
 
 router.use('/:slug', function(req, res, next) {
   var slug = req.params['slug'];
   var post = db.prepare('SELECT * from posts where slug = ?').get(slug);
-  var comments = commentsDB.prepare('SELECT * from comments where slug = ?').all(slug);
-  post.comments = comments;
+  var comments = commentsDB.prepare('SELECT rowid as ID, * from comments where slug = ?').all(slug);
+  post.comments = orderComments(comments);
 
   if(post) {
     res.render('content', post);
@@ -25,6 +48,23 @@ router.use('/:slug', function(req, res, next) {
     next();
   }
 });
+
+function orderComments(comments) {
+  var result = [];
+  var indexTable = {};
+  for(var i = 0; i < comments.length; i++) {
+    if (comments[i].parentID == 0) {
+      result.push(comments[i]);
+      indexTable[comments[i].ID] = result.length - 1;
+    } else {
+      var pi = indexTable[comments[i].parentID]; // pi: parent index
+      result[pi].children = result[pi].children ? result[pi].children : [];
+      result[pi].children.push(comments[i]);
+    }
+  }
+
+  return result;
+}
 
 router.use('/:view', function(req, res, next) {
   res.render(req.params['view']);
