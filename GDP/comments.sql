e2b5cc692585ad3641f4079bf288e0b5b9c71cdf
sainthkh <sainthkh@naver.com>
2018-09-24 08:40:55 +0900

    Added sql files.

---
+++ GDP/comments.sql
@@ -0,0 +1,9 @@
+create table(
+    comment text,
+    author varchar(128),
+    imageUrl varchar(1024),
+    url varchar(1024),
+    time char(256),
+    slug varchar(512),
+    parentID int
+);
---
+++ GDP/posts.sql
@@ -0,0 +1,8 @@
+create table(
+    title varchar(512),
+    description varchar(512),
+    content text,
+    slug varchar(256),
+    time char(256),
+    src varchar(512)
+);
