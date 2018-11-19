9db5d7460c6c124033398c280fc868b96de0b79d
sainthkh <sainthkh@naver.com>
2018-11-19 18:13:27 +0900

    Fixed imagelist to collect only unfinished image tags.

--- GDP/bin/imagelist.js
+++ GDP/bin/imagelist.js
@@ -1,3 +1,3 @@
 var fs = require('fs');
 var post = fs.readFileSync('./temp/post.md').toString();
-fs.writeFileSync('./temp/images.txt', post.match(/\!\[.*?\]/g).join('\n'));
+fs.writeFileSync('./temp/images.txt', post.match(/\!\[.*?\][\r\n]/g).join('\n'));
